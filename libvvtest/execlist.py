e07bfefa8ab4e11d533facb33d742f0a270bfeec
Richard R. Drake <rrdrake@sandia.gov>
2022-09-25 18:42:37 -0600

    move TestBacklog into its own file

--- libvvtest/execlist.py
+++ libvvtest/execlist.py
@@ -7,8 +7,8 @@
 import os, sys
 
 from .testexec import TestExec
-from . import depend
 from .teststatus import copy_test_results
+from .backlog import TestBacklog
 
 
 class TestExecList:
@@ -162,6 +162,8 @@ class TestExecList:
 
     def _generate_backlog_from_testlist(self):
         ""
+        # magic: have backlog store tcase (not TestExec)
+        # magic: move this function into TestBacklog
         for tcase in self.tlist.getTests():
             if not tcase.getStat().skipTest():
                 assert tcase.getSpec().constructionCompleted()
@@ -174,176 +176,10 @@ class TestExecList:
 
     def _pop_next_test(self, maxsize):
         ""
-        constraint = TestConstraint( maxsize )
-
-        texec = self.backlog.pop( constraint )
+        texec = self.backlog.pop_by_size( maxsize )
 
         if texec != None:
             tcase = texec.getTestCase()
             self.waiting[ tcase.getSpec().getID() ] = tcase
 
         return texec
-
-
-class TestConstraint:
-
-    def __init__(self, maxsize):
-        ""
-        self.maxsize = maxsize
-
-    def getMaxNP(self):
-        ""
-        if self.maxsize == None:
-            return None
-        else:
-            return self.maxsize[0]
-
-    def apply(self, texec):
-        ""
-        tcase = texec.getTestCase()
-
-        if self.maxsize != None:
-
-            np,nd = tcase.getSize()
-            maxnp,maxnd = self.maxsize
-
-            if np > maxnp or nd > maxnd:
-                return False
-
-        if tcase.isBlocked():
-            return False
-
-        return True
-
-
-class TestBacklog:
-    """
-    Stores a list of TestExec objects.  They can be sorted either by
-
-        ( num procs, runtime )
-    or
-        ( num procs, timeout ).
-
-    The former is used for pooled execution, while the latter for collecting
-    groups of tests for batching.
-    """
-
-    def __init__(self):
-        ""
-        self.tests = []
-        self.testcmp = None
-
-    def insert(self, texec):
-        """
-        Note: to support streaming, this function would have to use
-              self.testcmp to do an insert (rather than an append)
-        """
-        self.tests.append( texec )
-
-    def sort(self, secondary='runtime'):
-        ""
-        if secondary == 'runtime':
-            self.testcmp = TestExecCompare( make_runtime_key )
-        else:
-            assert secondary == 'timeout'
-            self.testcmp = TestExecCompare( make_timeout_key )
-
-        if sys.version_info[0] < 3:
-            self.tests.sort( self.testcmp.compare, reverse=True )
-        else:
-            self.tests.sort( key=self.testcmp.getKey, reverse=True )
-
-    def pop(self, constraint=None):
-        ""
-        texec = None
-
-        if constraint:
-            idx = self._get_starting_index( constraint.getMaxNP() )
-        else:
-            idx = 0
-
-        while idx < len( self.tests ):
-            if constraint == None or constraint.apply( self.tests[idx] ):
-                texec = self.tests.pop( idx )
-                break
-            idx += 1
-
-        return texec
-
-    def consume(self):
-        ""
-        while len( self.tests ) > 0:
-            texec = self.tests.pop( 0 )
-            yield texec
-
-    def iterate(self):
-        ""
-        for texec in self.tests:
-            yield texec
-
-    def _get_starting_index(self, max_np):
-        ""
-        if max_np == None:
-            return 0
-        else:
-            return bisect_left( self.tests, max_np )
-
-
-def make_runtime_key( texec ):
-    ""
-    tcase = texec.getTestCase()
-    return [ tcase.getSize()[0], tcase.getStat().getRuntime( 0 ) ]
-
-def make_timeout_key( texec ):
-    ""
-    tcase = texec.getTestCase()
-    return [ tcase.getSize()[0], tcase.getStat().getAttr( 'timeout' ) ]
-
-
-class TestExecCompare:
-    """
-    This class is a convenience for supporting Python 2 and 3 sorting.
-    Python 2 needs the compare function.  Python 3 just needs a "get key"
-    function, which could easily be done without this class.
-    """
-
-    def __init__(self, make_key):
-        ""
-        self.kfunc = make_key
-
-    def compare(self, x, y):
-        ""
-        k1 = self.kfunc(x)
-        k2 = self.kfunc(y)
-        if k1 < k2: return -1
-        if k2 < k1: return 1
-        return 0
-
-    def getKey(self, x):
-        ""
-        return self.kfunc( x )
-
-
-def bisect_left( tests, np ):
-    ""
-    lo = 0
-    hi = len(tests)
-    while lo < hi:
-        mid = (lo+hi)//2
-        if np < tests[mid].getTestCase().getSize()[0]: lo = mid+1
-        else: hi = mid
-    return lo
-
-# To insert into the sorted test list, a specialization of insort_right is
-# needed.  The comparison is based on np, and the list is in descending order.
-# This function is just the python implementation of bisect.insort_right()
-# and would need to be modified to work for this use case.
-# def insort_right( a, x, less_than ):
-#     ""
-#     lo = 0
-#     hi = len(a)
-#     while lo < hi:
-#         mid = (lo+hi)//2
-#         if less_than( x, a[mid] ): hi = mid
-#         else: lo = mid+1
-#     a.insert(lo, x)

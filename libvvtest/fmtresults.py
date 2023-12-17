7c5d70c1023e9655d4b6f13729c02edd2b618234
Richard R. Drake <rrdrake@sandia.gov>
2023-12-17 06:20:47 -0700

    implement better test path identification algorithm

--- libvvtest/fmtresults.py
+++ libvvtest/fmtresults.py
@@ -5,8 +5,11 @@
 # Government retains certain rights in this software.
 
 import os, sys
+from os.path import join as pjoin, basename, dirname
 import time
 
+# the test root marker filename
+VVTEST_ROOT_FILENAME = '.vvtest.root'
 
 # this is the file name of source tree runtimes files
 runtimes_filename = "runtimes"
@@ -735,6 +738,108 @@ def determine_rootrel( srcdir, dcache ):
         dcache[srcdir] = rootrel
 
     return rootrel
+
+
+class TestPathIdentification:
+    """
+    Note: The cache effectiveness could be improved by checking if the
+          incoming path is a subdir of a path in the cache.  However, it
+          may break if soft links are used in the test repository.
+    """
+
+    def __init__(self):
+        ""
+        self.cache = {}
+
+    def get_path_id(self, test_src_filename):
+        """
+        """
+        cache_d,bname = os.path.split( test_src_filename )
+
+        if cache_d in self.cache:
+            trail = self.cache[ cache_d ]
+        else:
+            d,b = os.path.split( os.path.realpath( test_src_filename ) )
+
+            trail = get_path_list_by_vvtest_root(d)
+
+            if trail is None:
+                trail = get_path_list_by_git_or_svn_repo(d)
+
+            # add to the cache even if identification fails
+            self.cache[ cache_d ] = trail
+
+        pathid = None
+        if trail is not None:
+            pathlist = trail + [bname]  # need to make a copy of trail here
+            pathid = pjoin( *pathlist )
+
+        return pathid
+
+
+def get_path_list_by_vvtest_root( filedir ):
+    """
+    returns a list of path segments from the .git or .svn directory to the
+    given 'filedir' directory
+    """
+    d = filedir
+    prev_d = None
+    trail = []
+
+    while d and d != prev_d and d != '/':
+
+        fn = pjoin(d,VVTEST_ROOT_FILENAME)
+        if os.path.exists(fn):
+            with open( fn, 'rt' ) as fp:
+                for line in fp:
+                    if parse_root_path( line, trail ):
+                        break
+            trail.reverse()
+            return trail
+
+        trail.append( basename(d) )
+        prev_d = d
+        d = dirname(d)
+
+    return None
+
+
+def parse_root_path( line, trail ):
+    """
+    parses 'line' for ROOTPATH=path and appends 'trail' with the path segments
+    returns True if the ROOTPATH was found
+    """
+    L = line.strip().split('=',1)
+    if len(L) == 2 and L[0].strip() == 'ROOTPATH':
+        d = L[1].strip()
+        if d and not os.path.isabs(d):
+            while d:
+                d,seg = os.path.split(d)
+                trail.append(seg)
+            return True
+    return False
+
+
+def get_path_list_by_git_or_svn_repo( filedir ):
+    """
+    returns a list of path segments from the .git or .svn directory to the
+    given 'filedir' directory
+    """
+    d = filedir
+    prev_d = None
+    trail = []
+
+    while d and d != prev_d and d != '/':
+
+        if os.path.exists(pjoin(d,'.git')) or os.path.exists(pjoin(d,'.svn')):
+            trail.reverse()
+            return trail
+
+        trail.append( basename(d) )
+        prev_d = d
+        d = dirname(d)
+
+    return None
 
 
 class LookupCache:
